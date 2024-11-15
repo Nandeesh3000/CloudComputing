f0d69190f2ff1629c24982aafc5d87b3331cad24
Nandeesh3000 <113001762+Nandeesh3000@users.noreply.github.com>
2024-11-15 20:02:24 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -4,12 +4,21 @@ import numpy as np
 import pandas as pd
 import sklearn
 from sklearn import linear_model
-height=[[3.0], [4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0], [11.0]]
-weight=[ 6, 8, 10, 12, 14, 16, 18, 20, 22]
-plt.scatter(height,weight,color='black')
-plt.xlabel("height")
-plt.ylabel("weight")
-reg=linear_model.LinearRegression()
-reg.fit(height,weight)
-X_height=[[182.0]]
-print(reg.predict(X_height))
+
+
+height = [[3.0], [4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0], [11.0]]
+weight = [6, 8, 10, 12, 14, 16, 18, 20, 22]
+
+
+plt.scatter([h[0] for h in height], weight, color='black')
+plt.xlabel("Height")
+plt.ylabel("Weight")
+
+reg.fit(height, weight)
+
+
+X_height = [[182.0]]
+predicted_weight = reg.predict(X_height)
+print(f"Predicted weight for height {X_height[0][0]}: {predicted_weight[0]}")
+
+plt.show()
